095ec2002156686541b861553315b94fc5e1d418
Pedro <pedrofreitas790@gmail.com>
2021-03-27 16:30:09 -0300

    Configuração do banco de dados heroku.

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('db_jymbo', 'root', 'root', {
-  dialect: 'mysql',
-  host: 'localhost'
+const sequelize = new Sequelize('d1ahq8km25276l', 'ilmthphzhgugpi', '5df426747370dcc611bc753cfbf7fa69138e9baa73a58238205c4e94743cd2d6', {
+  dialect: 'postgresql',
+  host: 'ec2-52-45-73-150.compute-1.amazonaws.com',
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 module.exports = sequelize;
