5d9bb0058ab947388ddc36d876de700affe0b9cf
Pedro <pedrofreitas790@gmail.com>
2021-03-28 14:27:01 -0300

    Usando variáveis de ambiente para acessar o banco.

--- src/database/database.js
+++ src/database/database.js
@@ -1,15 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('d1ahq8km25276l', 'ilmthphzhgugpi', '5df426747370dcc611bc753cfbf7fa69138e9baa73a58238205c4e94743cd2d6', {
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS, {
   dialect: 'postgresql',
-  host: 'ec2-52-45-73-150.compute-1.amazonaws.com',
+  host: process.env.DATABASE_HOST,
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     },
-  },
-
+  }
 });
 
 module.exports = sequelize;
