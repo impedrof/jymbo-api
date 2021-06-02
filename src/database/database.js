9bc29d93b738eabfb5e786be90ab7678c937f73f
Pedro Freitas <53201590+impedrof@users.noreply.github.com>
2021-06-01 23:47:06 -0300

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,6 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS, {
   dialect: 'postgres',
   host: process.env.DATABASE_HOST,
-  timezone: '-03:00',
   dialectOptions: {
     ssl: {
       require: true,
